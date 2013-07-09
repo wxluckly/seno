276c34ea752bc7464528a0c556b80426692a89c1
Joey wang <wxluckly@gmail.com>
2013-07-09 18:35:04 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seno::Application.config.secret_token = 'c938a572f5057333462c2123758016b78187ded590009e9f1e971b4d4acae1ab6cae555da927406e865892ce7f87d3117b4af27842e45a741756f3bb0c6cb286'
