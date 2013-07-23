dc186c3a8e0579f8bc4ffa9698196aee13aa1dbf
Joey wang <wxluckly@gmail.com>
2013-07-23 22:44:44 +0800

    add acts_as_list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710154656) do
+ActiveRecord::Schema.define(:version => 20130723144355) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130710154656) do
     t.integer  "sub_category_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.integer  "position"
   end
 
   create_table "categories", :force => true do |t|
